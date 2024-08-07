447c0d32a0d24f427defe61ec10695ba9f705def
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2024-08-07 15:30:00 +0530

    Updated the About Section

--- src/Compoenent/About-Event/About.jsx
+++ src/Compoenent/About-Event/About.jsx
@@ -9,13 +9,14 @@ const About = () => {
      <div className="Aboutcontent">
       <h1>International Conference on Advancement in Cyber Security and Digital Forensics</h1>
 
-      <p>The International Conference on Advances in Cybersecurity and Digital Forensics is a crucial event that brings <br/>together cybersecurity experts, digital forensics researchers, and information security professionals. This conference<br/> is dedicated to tackling the ever-changing challenges in cybersecurity and digital forensics, underscoring the need <br/>for advanced solutions in today's rapidly evolving technological landscape. Attendees will participate in <br/>discussions, share insights, and collaborate on the latest advancements in cybersecurity strategies, threat <br/>mitigation, and digital forensic techniques. The conference serves as a vibrant platform for exploring emerging <br/>trends, fostering interdisciplinary dialogue, and shaping the future of cybersecurity and digital forensics amidst <br/>evolving threats and digital crimes.</p>
+      <p>The International Conference on Advances in Cyber Security and Digital Forensics is a crucial event that brings <br/>together cybersecurity experts, digital forensics researchers, and information security professionals. This conference<br/> is dedicated to tackling the ever-changing challenges in cybersecurity and digital forensics, underscoring the need <br/>for advanced solutions in today's rapidly evolving technological landscape. Attendees will participate in <br/>discussions, share insights, and collaborate on the latest advancements in cybersecurity strategies, threat <br/>mitigation, and digital forensic techniques. The conference serves as a vibrant platform for exploring emerging <br/>trends, fostering interdisciplinary dialogue, and shaping the future of cybersecurity and digital forensics amidst <br/>evolving threats and digital crimes.</p>
      </div>
 
      <div className="event-schedule">
       <h1>Schedule</h1>
       <p>Review Time Plan: Dec 2024 - Jan 2025</p>
-     </div>
+     </div>cloud_download
+
 
       <div className="event-timeline">
         <div className="timeline-1">
