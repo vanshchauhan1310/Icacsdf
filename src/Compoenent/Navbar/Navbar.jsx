6d830222f6a1412a52069681268b38d31126be5c
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2024-08-21 20:45:48 +0530

    Made Navbar Responsive

--- src/Compoenent/Navbar/Navbar.jsx
+++ src/Compoenent/Navbar/Navbar.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleToggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className="Navbar-wrapper">
       <div className="Navbar-container">
@@ -9,7 +15,7 @@ const Navbar = () => {
           <h1 className="nav-heading">ICACSDF-25</h1>
         </div>
         <nav className="Navbar-links">
-          <ul>
+          <ul className={showMenu ? 'show' : ''}>
             <li><a href="/">Home</a></li>
             <li><a href="/about">About</a></li>
             <li><a href="/committee">Committee</a></li>
@@ -17,6 +23,11 @@ const Navbar = () => {
             <li><a href="/register">Registration</a></li>
             <li><a href="/contact">Contact-Us</a></li>
           </ul>
+          <div className="hamburger" onClick={handleToggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </nav>
       </div>
     </div>
