12d6f496de57c63e863c354dd5abc2954b16ec70
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2024-08-23 15:14:24 +0530

    Updated the team page

--- src/Compoenent/Team/Team.jsx
+++ src/Compoenent/Team/Team.jsx
@@ -1,21 +1,49 @@
 import React from 'react';
 import './Team.css';
-import teamData from './TeamData';
+import data from './data.json';
 
 const Team = () => {
+  const specialCategories = [
+    'National Advisory Board',
+    'International Advisory Board',
+    'Technical Program Committee'
+  ];
+
   return (
     <div className="Team-wrapper">
       <div className="Team">
-        {teamData.map((category, index) => (
-          <React.Fragment key={index}>
-            <h1 className='Comittee'>{category.category}</h1>
-            {category.members.map((member, memberIndex) => (
-              <React.Fragment key={memberIndex}>
-                {/* <h1 className='Position'>{member.position}</h1> */}
-                <h1 className='name'>{member.name} {member.position}</h1>
-              </React.Fragment>
-            ))}
-          </React.Fragment>
+        {Object.keys(data).map((category, index) => (
+          <div key={index}>
+            <h2 className='category'>{category}</h2>
+            {specialCategories.includes(category) ? (
+              // For special categories, render items directly without subcategories
+              Array.isArray(data[category]) ? (
+                <div>
+                  {data[category].map((item, itemIndex) => (
+                    <div className='nameofmember' key={itemIndex}>{item}</div>
+                  ))}
+                </div>
+              ) : (
+                <p className='nameofmember'>{data[category]}</p>
+              )
+            ) : (
+              // For other categories, render with subcategories
+              Object.keys(data[category]).map((subcategory, subindex) => (
+                <div key={subindex}>
+                  <h3 className='subcategory'>{subcategory}</h3>
+                  {Array.isArray(data[category][subcategory]) ? (
+                    <div>
+                      {data[category][subcategory].map((item, itemIndex) => (
+                        <div className='nameofmember' key={itemIndex}>{item}</div>
+                      ))}
+                    </div>
+                  ) : (
+                    <p className='nameofmember'>{data[category][subcategory]}</p>
+                  )}
+                </div>
+              ))
+            )}
+          </div>
         ))}
       </div>
     </div>
