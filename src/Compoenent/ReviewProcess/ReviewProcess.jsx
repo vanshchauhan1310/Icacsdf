9dfb35d1e206b30ade57d938e8c26ec64182d5a8
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2024-09-15 14:30:00 +0530

    Uploaded Image

--- src/Compoenent/ReviewProcess/ReviewProcess.jsx
+++ src/Compoenent/ReviewProcess/ReviewProcess.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import './Review.css'
+import workflow from '../ReviewProcess/workflow.jpeg'
 const ReviewProcess = () => {
   return (
     <>
+
+<div className="review-image-container">
+        <img 
+          src= {workflow}
+          alt="Review Process" 
+          className="review-image"
+        />
+      </div>
+
     </>
   )
 }
--- src/Pages/ReviewPage.jsx
+++ src/Pages/ReviewPage.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
+import ReviewProcess from '../Compoenent/ReviewProcess/ReviewProcess'
 
 const ReviewPage = () => {
   return (
-   <>
-        <div className="call-header">
-             <h1 className='call-heading'>Review Process</h1>
-        </div>
+    <>
+      <div className="call-header">
+        <h1 className='call-heading'>Review Process</h1>
+      </div>
 
-        <div className="workflow">
-            <img src='/public/images/workflow.jpeg'></img>
-        </div>
-
-   </>
+      <ReviewProcess/>
+    </>
   )
 }
 
